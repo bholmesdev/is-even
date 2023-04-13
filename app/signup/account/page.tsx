35111e5ce80f9e3cb2c78934d89ce9a08f8d4249
bholmesdev <bholmesdev@gmail.com>
2023-04-13 17:00:06 -0400

    feat: SQL in React??

--- app/signup/account/page.tsx
+++ app/signup/account/page.tsx
@@ -1,13 +1,17 @@
 import { redirect } from 'next/navigation';
 import { Form } from './Form';
 import { User, userSchema } from '../utils';
-import { writeUser } from '../mock-db';
+import { connect } from '@planetscale/database';
+
+const conn = connect({
+	url: process.env['DATABASE_URL'] || 'mysql://user:pass@host'
+});
 
 export default async function Account({ searchParams }: { searchParams: Record<string, string> }) {
 	if (Object.entries(searchParams).length) {
 		const parseRes = userSchema.safeParse(searchParams);
 		if (parseRes.success) {
-			await writeUser(parseRes.data);
+			await conn.execute('INSERT INTO users SET email= :email, name= :name;', parseRes.data);
 			redirect('/signup/confirm');
 		} else {
 			const { error } = parseRes;
--- app/signup/confirm/page.tsx
+++ app/signup/confirm/page.tsx
@@ -1,11 +1,21 @@
 import { readUser } from '../mock-db';
 import { redirect } from 'next/navigation';
+import { connect } from '@planetscale/database';
+
+const conn = connect({
+	url: process.env['DATABASE_URL'] || 'mysql://user:pass@host'
+});
 
 export default async function SignupConfirm() {
-	const user = await readUser();
-	if (!user) {
+	const res = await conn.execute('SELECT name, email FROM users WHERE email = :email;', {
+		email: 'ben@gmail.com'
+	});
+	if (!res.rows) {
 		redirect('/signup/account');
 	}
+
+	const [user] = res.rows;
+	console.log({ user });
 
 	return <h1>Confirmed! Welcome to is-even, {user.name} 👋</h1>;
 }
